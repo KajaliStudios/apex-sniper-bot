811c058e318796b3c1aff4ebbd4739440e6d72b2
KajaliStudios <kajalistudios@gmail.com>
2026-04-14 10:27:19 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+TOKEN = "8602858732:AAGV2AtJ-c3TdXQHBkrIH3fkPg96aGu0U-0"
+CHAT_ID = "1531088804"
+
+def send_telegram(message):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+    requests.post(url, json=payload)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    message = data.get("message", "No message")
+
+    send_telegram(message)
+
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
